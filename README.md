7c7936bf7e9b92d595dfa814485c14fbf5c13004
Jon Pastorek <jppastorek@gmail.com>
2024-10-20 22:39:17 -0500

    convert json to csv

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ This is a project to interact programatically with the Canvas LMS API
 Getting Started
 
 Step 1: Clone the repository to a directory on your local machine
-Step 2: Create a .env file and input your variables (this is more secure than hard coding endpoints and keys)
+Step 2: Create a .env file and input your tokens and other environment variables (this is more secure than hard coding endpoints and keys)
 Step 3: Install the necessary packages: pip install requests pandas google-auth google-auth-oauthlib google-auth-httplib2 google-python-api-client smtplib
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests, os
+import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,11 +13,13 @@ def get_canvas_data():
     headers = {
         'Authorization' : f'Bearer {CANVAS_API_TOKEN}'
     }
-    response = requests.get(f'{CANVAS_API_URL}/users', headers=headers)
+    response = requests.get(f'{CANVAS_API_URL}/users?per_page=1000', headers=headers)
     
     if response.status_code == 200:
         return response.json()
     else:
         raise Exception(f'Error fetching Canvas data: {response.status_code}, {response.text}')
 
-print(get_canvas_data())
+df = pd.DataFrame(get_canvas_data())
+
+df.to_csv('data.csv', index=False)
