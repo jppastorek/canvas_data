1deeefa434a22c233a65f40fbd3e3318bbf05ff5
Jon Pastorek <jppastorek@gmail.com>
2024-10-22 21:47:03 -0500

    added pagination functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, os
+import requests, os, time
 import pandas as pd
 from dotenv import load_dotenv
 
@@ -8,18 +8,49 @@ CANVAS_API_URL = os.environ.get('CANVAS_API_URL')
 CANVAS_API_TOKEN = os.environ.get('CANVAS_API_TOKEN')
 CANVAS_DEVELOPER_KEY = os.environ.get('CANVAS_DEVELOPER_KEY')
 
+canvas_url = f'{CANVAS_API_URL}/users?per_page=50'
 
-def get_canvas_data():
-    headers = {
-        'Authorization' : f'Bearer {CANVAS_API_TOKEN}'
-    }
-    response = requests.get(f'{CANVAS_API_URL}/users?per_page=1000', headers=headers)
+def get_canvas_data(url):
+    data_set = []
+    total_start_time = time.time()
+    total_items = 0
     
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception(f'Error fetching Canvas data: {response.status_code}, {response.text}')
+    while url:
+        start_time = time.time()
+        headers = {
+            'Authorization' : f'Bearer {CANVAS_API_TOKEN}'
+        }
+        response = requests.get(url, headers=headers)
+    
+        if response.status_code == 200:
+            link_header = response.headers.get('Link')
+            url = get_next_link(link_header)
+            data = response.json()
+            data_set.extend(data)
+            request_time = time.time() - start_time
+            print(f"Fetched {len(data)} items in {request_time:.2f} seconds.")
+            total_items += len(data)
+        else:
+            raise Exception(f'Error fetching Canvas data: {response.status_code}, {response.text}')
+    total_time = time.time() - total_start_time
+    print(f"Fetched {total_items} items.")
+    print(f"Total time taken to fetch all data: {total_time:.2f} seconds.")
+    return data_set
 
-df = pd.DataFrame(get_canvas_data())
+
+def get_next_link(link_header):
+    if not link_header:
+        return None
+    
+    links = {}
+    for part in link_header.split(','):
+        section = part.split(';')
+        url = section[0].strip()[1:-1] # Remove the angle brackets
+        rel = section[1].strip().split('=')[1].strip('"')
+        links[rel] = url
+    return links.get('next')
+
+
+df = pd.DataFrame(get_canvas_data(canvas_url))
 
 df.to_csv('data.csv', index=False)
